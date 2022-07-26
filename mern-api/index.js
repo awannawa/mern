e373b328e6b0a91f85a6cbd3484df224f5669331
awannawa <kurniawanofficiall@gmail.com>
2022-07-26 15:31:54 +0700

    27

--- mern-api/index.js
+++ mern-api/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const productRoutes = require("./src/routes/products");
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
 
-app.use("/v1/pelanggan", productRoutes);
+app.use(bodyParser.json()); //type JSON
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
 
 app.listen(4000);
---
+++ mern-api/src/controllers/blog.js
@@ -0,0 +1,21 @@
+exports.createBlogPost = (req, res, next) => {
+  const title = req.body.title;
+  // const image = req.body.image;
+  const image = req.body.body;
+
+  const result = {
+    message: "Create Blog Post Success",
+    data: {
+      post_id: 1,
+      title: "Title Blog",
+      //   image: "iamgepath.png",
+      body: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ",
+      created_at: "26/07/2022",
+      author: {
+        uid: 1,
+        name: "awan",
+      },
+    },
+  };
+  res.staus(201).json(result);
+};
