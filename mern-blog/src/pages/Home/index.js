d193a7ac0fe330cebba447a7e9c9cdf05fb2c32a
awannawa <kurniawanofficiall@gmail.com>
2022-07-28 20:43:36 +0700

    47 -> 48

--- mern-blog/src/pages/Home/index.js
+++ mern-blog/src/pages/Home/index.js
@@ -4,6 +4,7 @@ import "./home.scss";
 import { useHistory } from "react-router-dom";
 import Axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
+import { setDataBlog } from "../../config/redux/action";
 
 const Home = () => {
   const { dataBlog } = useSelector((state) => state.homeReducer);
@@ -11,15 +12,7 @@ const Home = () => {
 
   console.log("state blog global: ", dataBlog);
   useEffect(() => {
-    Axios.get("http://localhost:4000/v1/blog/posts?page=2&perPage=2")
-      .then((result) => {
-        const responseAPI = result.data;
-
-        dispatch({ type: "UPDATE_DATA_BLOG", payload: responseAPI.data });
-      })
-      .catch((err) => {
-        console.log("error: ", err);
-      });
+    dispatch(setDataBlog());
   }, [dispatch]);
   const history = useHistory();
   return (
